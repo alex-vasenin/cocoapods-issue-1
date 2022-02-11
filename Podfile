1d594b8d6244054fd76e13c676b951060193ccee
Alexander Vasenin <alex.vasenin@me.com>
2022-02-11 04:25:13 +0300

    Initial commit

---
+++ Podfile
@@ -0,0 +1,9 @@
+platform :ios, '13.0'
+use_frameworks!
+
+target 'test' do
+  pod 'Mute'
+  pod 'GoogleSignIn'
+  pod 'SDWebImage'
+end
+
---
+++ test/testApp.swift
@@ -0,0 +1,17 @@
+//
+//  testApp.swift
+//  test
+//
+//  Created by Alexander Vasenin on 11.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
